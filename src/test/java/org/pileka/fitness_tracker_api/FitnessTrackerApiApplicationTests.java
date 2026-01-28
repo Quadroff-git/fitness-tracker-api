324d950c0a0ca668c6e442af0b7d151c75e88445
Pavel Pileka <pavelpileka@gmail.com>
2026-01-28 18:21:29 +0300

    Initial commit

---
+++ src/test/java/org/pileka/fitness_tracker_api/FitnessTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pileka.fitness_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
