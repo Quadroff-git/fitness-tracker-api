65a4eceb6967d53a1998c5d38f22397d8a4bb7e1
Pavel Pileka <pavelpileka@gmail.com>
2026-01-28 19:01:00 +0300

    feat: add WorkoutType enum

---
+++ src/main/java/org/pileka/fitness_tracker_api/domain/WorkoutType.java
@@ -0,0 +1,17 @@
+package org.pileka.fitness_tracker_api.domain;
+
+public enum WorkoutType {
+    CARDIO,
+    STRENGTH,
+    YOGA,
+    PILATES,
+    CROSSFIT,
+    SWIMMING,
+    CYCLING,
+    RUNNING,
+    WALKING,
+    STRETCHING,
+    DANCE,
+    MARTIAL_ARTS,
+    OTHER
+}
