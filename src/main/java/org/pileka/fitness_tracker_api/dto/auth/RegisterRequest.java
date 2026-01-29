9d46113740f0d501d42510b09a08f2b5a0c47a32
Pavel Pileka <pavelpileka@gmail.com>
2026-01-29 15:33:49 +0300

    feat: add RegisterRequest DTO class

---
+++ src/main/java/org/pileka/fitness_tracker_api/dto/auth/RegisterRequest.java
@@ -0,0 +1,18 @@
+package org.pileka.fitness_tracker_api.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+}
