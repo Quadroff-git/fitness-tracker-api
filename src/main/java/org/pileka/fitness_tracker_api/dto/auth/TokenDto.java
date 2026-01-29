365f2242134b5bb392ff7bd2e853cdae5bcdfed6
Pavel Pileka <pavelpileka@gmail.com>
2026-01-29 19:10:35 +0300

    docs: document the unit in which expiration values are measured in TokenDto

--- src/main/java/org/pileka/fitness_tracker_api/dto/auth/TokenDto.java
+++ src/main/java/org/pileka/fitness_tracker_api/dto/auth/TokenDto.java
@@ -8,9 +8,15 @@ import lombok.Data;
 public class TokenDto {
     private String bearerToken;
 
+    /**
+     * The period the access token is valid for in milliseconds
+     */
     private Long accessTokenExpiration;
 
     private String refreshToken;
 
+    /**
+     * The period the refresh token is valid for in milliseconds
+     */
     private Long refreshTokenExpiration;
 }
