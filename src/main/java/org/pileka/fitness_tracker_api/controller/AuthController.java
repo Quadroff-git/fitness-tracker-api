ae6b568c4990a83e0c7158dac00c78be0278edc4
Pavel Pileka <pavelpileka@gmail.com>
2026-01-29 19:29:44 +0300

    feat: expand AuthController
    - Rename authenticate() to login()
    - Add refresh() method for access token refreshing
    - Change response structure for sending tokens
    - Refactor refresh() and login() to use a new helper method for generating a response from a TokenDto

--- src/main/java/org/pileka/fitness_tracker_api/controller/AuthController.java
+++ src/main/java/org/pileka/fitness_tracker_api/controller/AuthController.java
@@ -5,11 +5,11 @@ import org.pileka.fitness_tracker_api.dto.auth.LoginDto;
 import org.pileka.fitness_tracker_api.dto.auth.TokenDto;
 import org.pileka.fitness_tracker_api.dto.auth.RegistrationDto;
 import org.pileka.fitness_tracker_api.service.AuthService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -25,8 +25,34 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenDto> authenticate(@RequestBody LoginDto request) {
-        // Authenticate and return JWT
-        return ResponseEntity.ok(authService.login(request));
+    public ResponseEntity<String> login(@RequestBody LoginDto request) {
+        return getResponseFromTokenDto(authService.login(request));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<String> refresh(@CookieValue("refresh_token") String refreshToken) {
+        TokenDto tokenDto = authService.refresh(refreshToken);
+        if (tokenDto != null) {
+            return getResponseFromTokenDto(tokenDto);
+        }
+        else {
+            return ResponseEntity.status(HttpStatusCode.valueOf(401)).build();
+        }
+    }
+
+    private ResponseEntity<String> getResponseFromTokenDto(TokenDto tokenDto) {
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refresh_token", tokenDto.getRefreshToken())
+                .httpOnly(true)
+                .secure(true)
+                .path("/api/auth/refresh")
+                .maxAge(tokenDto.getRefreshTokenExpiration() / 1000)
+                .sameSite("strict")
+                .build();
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
+                .body(String.format("""
+                            "access_token" : %s,
+                            "expires_in" : %d
+                            """, tokenDto.getBearerToken(), tokenDto.getAccessTokenExpiration() / 1000));
     }
 }
