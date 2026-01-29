1ad0e9b4f251757024a493f22faa25f6659f9bc9
Pavel Pileka <pavelpileka@gmail.com>
2026-01-29 15:54:47 +0300

    refactor: rename RegisterRequest class to RegistrationRequest

--- src/main/java/org/pileka/fitness_tracker_api/dto/auth/RegistrationRequest.java
+++ src/main/java/org/pileka/fitness_tracker_api/dto/auth/RegistrationRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class RegisterRequest {
+public class RegistrationRequest {
     @NotBlank(message = "Username is required")
     private String username;
 
