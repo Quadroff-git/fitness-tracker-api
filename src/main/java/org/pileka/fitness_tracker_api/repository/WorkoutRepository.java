ac0b8e58671e8dc4e9ce901d3c6bd5dd7fe94b81
Pavel Pileka <pavelpileka@gmail.com>
2026-01-29 10:23:34 +0300

    feat: add 2 findByUser overloads to WorkoutRepository

--- src/main/java/org/pileka/fitness_tracker_api/repository/WorkoutRepository.java
+++ src/main/java/org/pileka/fitness_tracker_api/repository/WorkoutRepository.java
@@ -1,8 +1,15 @@
 package org.pileka.fitness_tracker_api.repository;
 
+import org.pileka.fitness_tracker_api.domain.User;
 import org.pileka.fitness_tracker_api.domain.Workout;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface WorkoutRepository extends ListCrudRepository<Workout, Long>, PagingAndSortingRepository<Workout, Long>, JpaSpecificationExecutor<Workout> {}
+import java.util.List;
+
+public interface WorkoutRepository extends ListCrudRepository<Workout, Long>, PagingAndSortingRepository<Workout, Long>, JpaSpecificationExecutor<Workout> {
+    List<Workout> findByUser(User user);
+    List<Workout> findByUser(User user, Pageable pageable);
+}
