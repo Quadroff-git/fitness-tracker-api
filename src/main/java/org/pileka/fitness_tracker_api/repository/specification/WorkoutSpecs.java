626d73d11443c2d569427b13eeee2f6b28781829
Pavel Pileka <pavelpileka@gmail.com>
2026-01-30 14:55:39 +0300

    feat: add WorkoutSpecs convenience class

---
+++ src/main/java/org/pileka/fitness_tracker_api/repository/specification/WorkoutSpecs.java
@@ -0,0 +1,68 @@
+package org.pileka.fitness_tracker_api.repository.specification;
+
+import org.pileka.fitness_tracker_api.domain.User;
+import org.pileka.fitness_tracker_api.domain.Workout;
+import org.pileka.fitness_tracker_api.domain.WorkoutType;
+import org.springframework.data.jpa.domain.PredicateSpecification;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+/**
+ * A convenience class for getting specification objects without the service interacting with Spring Data-specific API
+ * All intervals are inclusive on both ends
+ */
+public class WorkoutSpecs {
+
+    public static Specification<Workout> belongsToUser(User user) {
+        return (from, query, builder) -> builder.equal(from.get("user"), user);
+    }
+
+    public static Specification<Workout> isOfType(WorkoutType type) {
+        return (from, query, builder) -> builder.equal(from.get("type"), type);
+    }
+
+    public static Specification<Workout> happenedAfter(LocalDate date) {
+        return (from, query, builder) -> builder.greaterThanOrEqualTo(from.get("date"), date);
+    }
+
+    public static Specification<Workout> happenedBefore(LocalDate date) {
+        return (from, query, builder) -> builder.lessThanOrEqualTo(from.get("date"), date);
+    }
+
+    public static Specification<Workout> isLongerThan(Integer duration) {
+        return (from, query, builder) -> builder.greaterThanOrEqualTo(from.get("duration"), duration);
+    }
+
+    public static Specification<Workout> isShorterThan(Integer duration) {
+        return (from, query, builder) -> builder.lessThanOrEqualTo(from.get("duration"), duration);
+    }
+
+    public static Specification<Workout> getFullSpec(User user,
+                                                              Optional<WorkoutType> type,
+                                                              Optional<LocalDate> startDate,
+                                                              Optional<LocalDate> endDate,
+                                                              Optional<Integer> minDuration,
+                                                              Optional<Integer> maxDuration) {
+        Specification<Workout> predicate = belongsToUser(user);
+        if (type.isPresent()) {
+            predicate = predicate.and(isOfType(type.get()));
+        }
+        if (startDate.isPresent()) {
+            predicate = predicate.and(happenedAfter(startDate.get()));
+        }
+        if (endDate.isPresent()) {
+            predicate = predicate.and(happenedBefore(endDate.get()));
+        }
+        if (minDuration.isPresent()) {
+            predicate = predicate.and(isLongerThan(minDuration.get()));
+        }
+        if (maxDuration.isPresent()) {
+            predicate = predicate.and(isShorterThan(maxDuration.get()));
+        }
+
+        return predicate;
+    }
+}
+
