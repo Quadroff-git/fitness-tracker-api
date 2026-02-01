e4e30ede97190f86c6a22b285605e328ec192737
Pavel Pileka <pavelpileka@gmail.com>
2026-02-01 11:48:12 +0300

    feat: add MediaDto class

---
+++ src/main/java/org/pileka/fitness_tracker_api/dto/media/MediaDto.java
@@ -0,0 +1,14 @@
+package org.pileka.fitness_tracker_api.dto.media;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MediaDto {
+    @NotEmpty
+    private byte[] image;
+}
