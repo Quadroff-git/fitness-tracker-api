b51e021767e687d86f9c306091193f026e4e88f9
Pavel Pileka <pavelpileka@gmail.com>
2026-02-01 13:30:29 +0300

    fix: Add @NoRepositoryBean in BaseRepository and add MediaRepository interface
    - this fixes the problem where Spring attempts to create a generic BaseRepository and can't because Object class isn't managed by JPA. Another way of fixing that would be to add an entity superclass, but this works too.

---
+++ src/main/java/org/pileka/fitness_tracker_api/repository/MediaRepository.java
@@ -0,0 +1,6 @@
+package org.pileka.fitness_tracker_api.repository;
+
+import org.pileka.fitness_tracker_api.domain.Media;
+
+public interface MediaRepository extends BaseRepository<Media, Long> {
+}
