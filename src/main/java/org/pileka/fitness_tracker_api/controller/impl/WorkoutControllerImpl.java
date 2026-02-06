b89c34a5e5e9ace8a9062e47f36dc2fbef853d4a
Pavel Pileka <pavelpileka@gmail.com>
2026-02-06 18:42:00 +0300

    feat: extract an interface from WorkoutController
    Implementation renamed to WorkoutControllerImpl, interface is WorkoutController

---
+++ src/main/java/org/pileka/fitness_tracker_api/controller/impl/WorkoutControllerImpl.java
@@ -0,0 +1,93 @@
+package org.pileka.fitness_tracker_api.controller.impl;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.pileka.fitness_tracker_api.controller.WorkoutController;
+import org.pileka.fitness_tracker_api.domain.WorkoutType;
+import org.pileka.fitness_tracker_api.dto.workout.CreateUpdateWorkoutDto;
+import org.pileka.fitness_tracker_api.dto.workout.ReadWorkoutDto;
+import org.pileka.fitness_tracker_api.service.WorkoutService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/workouts")
+@RequiredArgsConstructor
+public class WorkoutControllerImpl implements WorkoutController {
+
+    private final WorkoutService workoutService;
+
+    @GetMapping(produces = "application/json")
+    @Override
+    public Page<ReadWorkoutDto> getWorkouts(@AuthenticationPrincipal UserDetails userDetails,
+                                     @RequestParam Optional<WorkoutType> type,
+                                     @RequestParam Optional<LocalDate> startDate,
+                                     @RequestParam Optional<LocalDate> endDate,
+                                     @RequestParam Optional<Integer> minDuration,
+                                     @RequestParam Optional<Integer> maxDuration,
+                                     @PageableDefault(size = 20, sort = "date", direction = Sort.Direction.DESC) Pageable pageable) {
+        return workoutService.findAll(userDetails,
+                type,
+                startDate,
+                endDate,
+                minDuration,
+                maxDuration,
+                pageable);
+    }
+
+    @GetMapping(path = "/{id}", produces = "application/json")
+    @Override
+    public ResponseEntity<ReadWorkoutDto> getWorkoutById(@AuthenticationPrincipal UserDetails userDetails,
+                                                  @PathVariable Long id) {
+        Optional<ReadWorkoutDto> workout = workoutService.findById(id, userDetails);
+        if (workout.isPresent()) {
+            return ResponseEntity.ok(workout.get());
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping(produces = "application/json")
+    @Override
+    public ReadWorkoutDto addWorkout(@AuthenticationPrincipal UserDetails userDetails,
+                              @Valid @RequestBody CreateUpdateWorkoutDto createDto) {
+        return workoutService.create(createDto, userDetails);
+    }
+
+    @PutMapping(path = "/{id}", produces = "application/json")
+    @Override
+    public ResponseEntity<ReadWorkoutDto> updateWorkout(@AuthenticationPrincipal UserDetails userDetails,
+                                                 @PathVariable Long id,
+                                                 @Valid @RequestBody CreateUpdateWorkoutDto updateDto) {
+        Optional<ReadWorkoutDto> updatedWorkout = workoutService.update(id, userDetails, updateDto);
+        if (updatedWorkout.isPresent()) {
+            return ResponseEntity.ok(updatedWorkout.get());
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping(path = "/{id}", produces = "application/json")
+    @Override
+    public ResponseEntity<ReadWorkoutDto> deleteWorkout(@AuthenticationPrincipal UserDetails userDetails,
+                                                 @PathVariable Long id) {
+        Optional<ReadWorkoutDto> deletedWorkout = workoutService.delete(id, userDetails);
+        if (deletedWorkout.isPresent()) {
+            return ResponseEntity.ok(deletedWorkout.get());
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
