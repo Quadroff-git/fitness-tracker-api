189af2736279d41f9ddf0dc235764377e0517626
Pavel Pileka <pavelpileka@gmail.com>
2026-02-07 13:15:30 +0300

    feat: configure UserMapper to use password encoding
    UserMapper is now an abstract class and has a PasswordEncoder dependency

--- src/main/java/org/pileka/fitness_tracker_api/mapper/UserMapper.java
+++ src/main/java/org/pileka/fitness_tracker_api/mapper/UserMapper.java
@@ -1,11 +1,19 @@
 package org.pileka.fitness_tracker_api.mapper;
 
+import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.pileka.fitness_tracker_api.domain.User;
 import org.pileka.fitness_tracker_api.dto.auth.RegistrationDto;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@RequiredArgsConstructor
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
-public interface UserMapper {
-    User toModel(RegistrationDto dto);
+public abstract class UserMapper {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Mapping(target = "password", expression = "java(passwordEncoder.encode(dto.getPassword())")
+    public abstract User toModel(RegistrationDto dto);
 }
