bd6ef7248482f467ab96ad990bf0a6ec21425f8f
Pavel Pileka <pavelpileka@gmail.com>
2026-02-07 11:28:07 +0300

    feat: add MediaMapper interface

---
+++ src/main/java/org/pileka/fitness_tracker_api/mapper/MediaMapper.java
@@ -0,0 +1,15 @@
+package org.pileka.fitness_tracker_api.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.pileka.fitness_tracker_api.domain.Media;
+import org.pileka.fitness_tracker_api.domain.User;
+import org.pileka.fitness_tracker_api.dto.media.MediaDto;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface MediaMapper {
+
+    @Mapping(target = "id", expression = "java(null)") // maps user.id to media.id without this line
+    Media toModel(MediaDto dto, User user);
+}
