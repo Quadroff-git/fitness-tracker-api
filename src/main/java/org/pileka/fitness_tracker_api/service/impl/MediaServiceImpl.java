3a5732b04b6f5dc067c39c4d287cb42f64fcc6e6
Pavel Pileka <pavelpileka@gmail.com>
2026-02-07 13:05:14 +0300

    feat: refactor MediaServiceImpl to use MediaMapper

--- src/main/java/org/pileka/fitness_tracker_api/service/impl/MediaServiceImpl.java
+++ src/main/java/org/pileka/fitness_tracker_api/service/impl/MediaServiceImpl.java
@@ -1,9 +1,9 @@
 package org.pileka.fitness_tracker_api.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.pileka.fitness_tracker_api.domain.Media;
 import org.pileka.fitness_tracker_api.dto.media.MediaDto;
+import org.pileka.fitness_tracker_api.mapper.MediaMapper;
 import org.pileka.fitness_tracker_api.repository.UserRepository;
 import org.pileka.fitness_tracker_api.repository.MediaRepository;
 import org.pileka.fitness_tracker_api.service.MediaService;
@@ -16,12 +16,11 @@ public class MediaServiceImpl implements MediaService {
 
     private final MediaRepository mediaRepository;
     private final UserRepository userRepository;
-    private final ModelMapper modelMapper;
+    private final MediaMapper mediaMapper;
 
     public MediaDto create(MediaDto createDto, UserDetails userDetails) {
-        Media newMedia = modelMapper.map(createDto, Media.class);
-        newMedia.setUser(userRepository.findByUsername(userDetails.getUsername()).get());
+        Media newMedia = mediaMapper.toModel(createDto, userRepository.findByUsername(userDetails.getUsername()).get());
 
-        return modelMapper.map(mediaRepository.save(newMedia), MediaDto.class);
+        return mediaMapper.toDto(mediaRepository.save(newMedia));
     }
 }
