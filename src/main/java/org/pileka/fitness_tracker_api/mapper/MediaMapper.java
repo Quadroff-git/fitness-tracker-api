5facd6d75822c5b5cc152c1f81b22a537517965d
Pavel Pileka <pavelpileka@gmail.com>
2026-02-08 17:54:09 +0300

    feat: add checked exception to MediaMapper.toModel()

--- src/main/java/org/pileka/fitness_tracker_api/mapper/MediaMapper.java
+++ src/main/java/org/pileka/fitness_tracker_api/mapper/MediaMapper.java
@@ -7,10 +7,12 @@ import org.pileka.fitness_tracker_api.domain.Media;
 import org.pileka.fitness_tracker_api.domain.User;
 import org.pileka.fitness_tracker_api.dto.media.MediaDto;
 
+import java.io.IOException;
+
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface MediaMapper {
 
     @Mapping(target = "id", expression = "java(null)") // maps user.id to media.id without this line
     @Mapping(target = "image", expression = "java(dto.getBytes())")
-    Media toModel(MediaDto dto, User user);
+    Media toModel(MediaDto dto, User user) throws IOException;
 }
