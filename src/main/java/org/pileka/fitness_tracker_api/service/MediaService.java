9d1ba28bcd16a52604c36f10a0ad58fb1850b2e6
Pavel Pileka <pavelpileka@gmail.com>
2026-02-08 17:42:26 +0300

    feat: change MediaService.create() to return void

--- src/main/java/org/pileka/fitness_tracker_api/service/MediaService.java
+++ src/main/java/org/pileka/fitness_tracker_api/service/MediaService.java
@@ -3,5 +3,5 @@ package org.pileka.fitness_tracker_api.service;
 import org.pileka.fitness_tracker_api.dto.media.MediaDto;
 
 public interface MediaService {
-    MediaDto create(MediaDto createDto);
+    void create(MediaDto createDto);
 }
