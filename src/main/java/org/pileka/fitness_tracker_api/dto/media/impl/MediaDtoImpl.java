b98d73c8294cb90d1d9f3eed9a68434413123df5
Pavel Pileka <pavelpileka@gmail.com>
2026-02-08 14:43:26 +0300

    feat: add MediaDtoImpl class

---
+++ src/main/java/org/pileka/fitness_tracker_api/dto/media/impl/MediaDtoImpl.java
@@ -0,0 +1,29 @@
+package org.pileka.fitness_tracker_api.dto.media.impl;
+
+import lombok.AllArgsConstructor;
+import org.pileka.fitness_tracker_api.dto.media.MediaDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+// A Spring-specific implementation using MutlipartFile
+@AllArgsConstructor
+public class MediaDtoImpl implements MediaDto {
+
+    private MultipartFile file;
+
+    @Override
+    public String getType() {
+        return file.getContentType();
+    }
+
+    @Override
+    public long getSize() {
+        return file.getSize();
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        return file.getBytes();
+    }
+}
