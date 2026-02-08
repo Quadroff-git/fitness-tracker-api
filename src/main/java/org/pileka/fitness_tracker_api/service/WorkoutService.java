27de15e5ac2284e657704966ec3557f0fdc53ac5
Pavel Pileka <pavelpileka@gmail.com>
2026-02-08 11:38:22 +0300

    feat: remove UserDetails parameters from WorkoutService
    BREAKING CHANGE: implementation is broken now

--- src/main/java/org/pileka/fitness_tracker_api/service/WorkoutService.java
+++ src/main/java/org/pileka/fitness_tracker_api/service/WorkoutService.java
@@ -17,25 +17,22 @@ public interface WorkoutService {
      * Create a new workout belonging to user
      *
      * @param createDto DTO containing values for the new workout
-     * @param userDetails security credentials of the user to whom the workout will belong
      * @return A DTO representing the newly created workout
      */
-    ReadWorkoutDto create(CreateUpdateWorkoutDto createDto, UserDetails userDetails);
+    ReadWorkoutDto create(CreateUpdateWorkoutDto createDto);
 
     /**
      * Get user's workout by id
      *
      * @param id workout id
-     * @param userDetails security credentials of the user to whom the workout must belong
      * @return workout DTO or null wrapped in Optional if no workout
      * with this id belonging to this userDetails is found
      * */
-    public Optional<ReadWorkoutDto> findById(Long id, UserDetails userDetails);
+    public Optional<ReadWorkoutDto> findById(Long id);
 
     /**
      * Get userDetails's workouts, optionally according to some filters
      *
-     * @param userDetails security credentials of the user to whom the workout must belong
      * @param type string representing one of the WorkoutType values
      * @param startDate start of the date interval
      * @param endDate end of the date interval
@@ -44,8 +41,7 @@ public interface WorkoutService {
      * @return List of workout DTOs that fit the filters
      */
     // Using Optional in parameters because that's what we get from the request in the controllers
-    public List<ReadWorkoutDto> findAll(UserDetails userDetails,
-                                        Optional<WorkoutType> type,
+    public List<ReadWorkoutDto> findAll(Optional<WorkoutType> type,
                                         Optional<LocalDate> startDate,
                                         Optional<LocalDate> endDate,
                                         Optional<Integer> minDuration,
@@ -54,7 +50,6 @@ public interface WorkoutService {
     /**
      * Get user's workouts with pagination, optionally according to filters
      *
-     * @param userDetails security credentials of the user to whom the workout must belong
      * @param type string representing one of the WorkoutType values
      * @param startDate start of the date interval
      * @param endDate end of the date interval
@@ -63,8 +58,7 @@ public interface WorkoutService {
      * @param pageable pagination configuration
      * @return Page of workout DTOs that fit the filters
      */
-    Page<ReadWorkoutDto> findAll(UserDetails userDetails,
-                                        Optional<WorkoutType> type,
+    Page<ReadWorkoutDto> findAll(Optional<WorkoutType> type,
                                         Optional<LocalDate> startDate,
                                         Optional<LocalDate> endDate,
                                         Optional<Integer> minDuration,
@@ -75,20 +69,18 @@ public interface WorkoutService {
      * Update user's workout by id
      *
      * @param id id of the workout to update
-     * @param userDetails security credentials of the user to whom the workout must belong
      * @param updateDto DTO containing new values for the workout
      * @return updated workout DTO or null wrapped in Optional if no workout
      * with this id belonging to this userDetails is found
      * */
-    Optional<ReadWorkoutDto> update(Long id, UserDetails userDetails, CreateUpdateWorkoutDto updateDto);
+    Optional<ReadWorkoutDto> update(Long id, CreateUpdateWorkoutDto updateDto);
 
     /**
      * Delete user's workout by id
      *
      * @param id id of the workout to delete
-     * @param userDetails security credentials of the user to whom the workout must belong
      * @return deleted workout DTO or null wrapped in Optional if no workout
      * with this id belonging to this userDetails is found
      */
-    Optional<ReadWorkoutDto> delete(Long id, UserDetails userDetails);
+    Optional<ReadWorkoutDto> delete(Long id);
 }
