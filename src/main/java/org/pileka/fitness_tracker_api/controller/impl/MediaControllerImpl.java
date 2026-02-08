18c2d35a3224982444fa0cc9faf19c63d4b23318
Pavel Pileka <pavelpileka@gmail.com>
2026-02-08 17:43:39 +0300

    feat: move all the logic out of MediaControllerImpl

--- src/main/java/org/pileka/fitness_tracker_api/controller/impl/MediaControllerImpl.java
+++ src/main/java/org/pileka/fitness_tracker_api/controller/impl/MediaControllerImpl.java
@@ -2,11 +2,9 @@ package org.pileka.fitness_tracker_api.controller.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.pileka.fitness_tracker_api.controller.MediaController;
-import org.pileka.fitness_tracker_api.dto.media.MediaDto;
+import org.pileka.fitness_tracker_api.dto.media.impl.MediaDtoImpl;
 import org.pileka.fitness_tracker_api.service.MediaService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,23 +19,7 @@ public class MediaControllerImpl implements MediaController {
     @PostMapping(produces = "plain/text", consumes="multipart/form-data")
     @Override
     public ResponseEntity<String> addMedia(@RequestParam MultipartFile image) {
-        if (image.getContentType() == null || !image.getContentType().startsWith("image/")) {
-            return ResponseEntity.badRequest().body("Only image uploading is allowed");
-        }
-
-        if (image.getSize() > 10 * 1024 * 1024) {
-            return ResponseEntity.badRequest().body("Image size exceeds 10MB limit");
-        }
-
-        MediaDto mediaDto;
-        try {
-            mediaDto = new MediaDto(image.getBytes());
-        } catch (IOException e) {
-            return ResponseEntity.internalServerError().body("Something failed when reading the image");
-        }
-
-        mediaService.create(mediaDto);
-
-        return ResponseEntity.ok("Saved successfully");
+        mediaService.create(new MediaDtoImpl(image));
+        return ResponseEntity.ok().build();
     }
 }
